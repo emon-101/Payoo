124ad66f70a9e71313c6b0256bd56ada5d22f58c
emon-101 <emon26.ugv@gmail.com>
2026-02-21 11:03:31 +0600

    write logics for add money section

--- scripts/addMoney.js
+++ scripts/addMoney.js
@@ -1,1 +1,35 @@
-console.log('file successfully connected');
+document.getElementById('add-money-btn').addEventListener('click', function(){
+    // select the bank
+    const bank = getInputValue('select-bank');
+    if(bank == 'Select Bank') {
+        alert('Please select a bank!');
+        return;
+    }
+    // get the bank account number
+    const backAccount = getInputValue('account-number');
+    if(backAccount.length != 11) {
+        alert('Incorrect account number!');
+        return;
+    }
+
+    // get the amount
+    const amount = getInputValue('add-amount');
+    if(amount <0 ) {
+        alert('Invalid balance!');
+        return;
+    }
+
+    // get the current balance
+    const currentBalance = getBalance() + Number(amount);
+
+    // get the pin number and execute the add money program
+    const pin = getInputValue('add-money-pin');
+
+    if(pin=='1001') {
+        alert(`Add Money Successful form ${bank} at ${new Date()}`);
+        setBalance(currentBalance);
+    } else {
+        alert('Invalid Pin number');
+        return;
+    }
+})
